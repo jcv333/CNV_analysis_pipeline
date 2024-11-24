86dee1c69643e8a4352582a8ce93cc978e34cf93
Jose Camacho Valenzuela <jcamachov_23@hotmail.com>
2024-11-24 07:05:21 -0600

    pileup.sh

--- 01_PileUp/pileup.sh
+++ 01_PileUp/pileup.sh
@@ -3,11 +3,13 @@
 # 1. Load modules.
 module load samtools/1.10
 module load tabix/0.2.6
+module load r/4.2.2
 
 # 2. Create variables for directories.
 ref=/home/path/to/dir/hg19_reference
 source=/home/path/to/dir/source
 sequenza=/home/path/to/dir/Sequenza
+source_script=/home/path/to/dir/scripts
 
 # 3. Run Sequenza - pileup.
 # ----- a) Generate wiggle file - hg19 reference.
@@ -29,5 +31,8 @@ sequenza-utils seqz_binning \
 --seqz $source/bam2_out.seqz.gz \
 -w XX \
 -o $source/bam2_out_small.seqz.gz
+
+# 4. Execute the Rscript for segment analysis.
+Rscript $source_script/segments_analysis.sh
 
 # The end.
