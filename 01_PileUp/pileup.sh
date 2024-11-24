8b06e55052a77e89692fe9dd92fb7edbcbf2df7a
Jose Camacho Valenzuela <jcamachov_23@hotmail.com>
2024-11-24 06:41:54 -0600

    pileup.sh

---
+++ 01_PileUp/pileup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# 1. Load modules.
+module load samtools/1.10
+module load tabix/0.2.6
+
+# 2. Create variables for directories.
+ref=/home/path/to/dir/hg19_reference
+source=/home/path/to/dir/source
+sequenza=/home/path/to/dir/Sequenza
+
+# 3. Run Sequenza - pileup.
+# ----- a) Generate wiggle file - hg19 reference.
+sequenza-utils gc_wiggle \
+--fasta $ref/Homo_sapiens.hg19.fa \
+-o $source/Sequenza_universal_hg19.wig.gz
+
+# ---------- b) Generate seqz file with CNV numerical data.
+sequenza-utils bam2seqz \
+-n $source/bam1_sorted_nodup_aln.bam \
+-t $source/bam2_sorted_nodup_aln.bam \
+--fasta $ref/Homo_sapiens.hg19.fa \
+-gc $source/Sequenza_universal_hg19.wig.gz \
+-o $source/bam2_out.seqz.gz \
+--samtools /home/path/to/dir/miniconda3/bin/samtools
+
+# ---------- c) Generate output, which will be the input for the subsequent CNV analysis in R.
+sequenza-utils seqz_binning \
+--seqz $source/bam2_out.seqz.gz \
+-w XX \
+-o $source/bam2_out_small.seqz.gz
+
+# The end.
